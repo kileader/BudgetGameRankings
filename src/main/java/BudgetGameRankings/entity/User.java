7a48f8b0699a6012ab1bcbc38a2c7872758990d3
kileader <kevin.i.leader@gmail.com>
2021-02-13 16:21:28 -0600

    Create java class structure and first javabean

---
+++ src/main/java/BudgetGameRankings/entity/User.java
@@ -0,0 +1,86 @@
+package BudgetGameRankings.entity;
+
+
+/**
+ * A javabean class to represent a user.
+ *
+ * @author kileader
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private int settingsID;
+    private int wishlistID;
+
+    public User() {
+    }
+
+    public User(int id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSettingsID() {
+        return settingsID;
+    }
+
+    public void setSettingsID(int settingsID) {
+        this.settingsID = settingsID;
+    }
+
+    public int getWishlistID() {
+        return wishlistID;
+    }
+
+    public void setWishlistID(int wishlistID) {
+        this.wishlistID = wishlistID;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", settingsID=" + settingsID +
+                ", wishlistID=" + wishlistID +
+                '}';
+    }
+}
