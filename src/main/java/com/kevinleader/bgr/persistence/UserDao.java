e2bca38999bbea2a871ee34e159ddced50214162
kileader <kevin.i.leader@gmail.com>
2021-02-14 14:57:51 -0600

    Work more on hibernate integration

--- src/main/java/com/kevinleader/bgr/persistence/UserDao.java
+++ src/main/java/com/kevinleader/bgr/persistence/UserDao.java
@@ -4,13 +4,45 @@ import com.kevinleader.bgr.entity.User;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class UserDao {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
-    public List<
+    public List<User> getAllUsers() {
+        logger.info("run getAllUsers");
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+        session.close();
+        return users;
+    }
+
+    public List<User> getUsersByUsername(String userName){
+        logger.info("run getUsersByUserName()");
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        Expression<String> propertyPath = root.get("userName");
+        query.where(builder.like(propertyPath, "%" + userName + "%"));
+        List<User> users = session.createQuery(query).getResultList();
+        session.close();
+        return users;
+    }
+
+}
 
 }
