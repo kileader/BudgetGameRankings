504c97e7e97130a3f27b29a88eb0e87fad76de5b
kileader <kevin.i.leader@gmail.com>
2021-02-14 11:19:59 -0600

    Add SessionFactoryProvider. Work on misc. hibernate integration

--- src/main/java/com/kevinleader/bgr/persistence/UserDao.java
+++ src/main/java/com/kevinleader/bgr/persistence/UserDao.java
@@ -1,4 +1,16 @@
 package com.kevinleader.bgr.persistence;
 
+import com.kevinleader.bgr.entity.User;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+
 public class UserDao {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public List<
+
 }
