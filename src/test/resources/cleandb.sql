81d5620a893472c837365276218366a5b264f4c4
kileader <kevin.i.leader@gmail.com>
2021-02-14 23:34:40 -0500

    Add cleandb.sql for resetting test user table

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,7 @@
+delete from user;
+insert into user values (0, 'jcoyne', 'coynemcgoin@hotmail.com', 'supersecret1');
+insert into user values (0, 'fhensen', 'scoobydoolover@yahoo.com', 'supersecret2');
+insert into user values (0, 'bcurry', 'baconcurry@gmail.com', 'supersecret3');
+insert into user values (0, 'kmack', 'mackattack@valdosta.edu', 'supersecret4');
+insert into user values (0, 'dklein', 'inkleined@madisoncollege.edu', 'supersecret5');
+insert into user values (0, 'dtillman', 'xxtillmanxx@hotmail.com', 'supersecret6');
