f18caa6add686db3c5aa6b74017802a17922682d
kileader <kevin.i.leader@gmail.com>
2021-02-14 23:56:08 -0500

    got

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,7 +1,7 @@
 delete from user;
-insert into user values (0, 'jcoyne', 'coynemcgoin@hotmail.com', 'supersecret1');
-insert into user values (0, 'fhensen', 'scoobydoolover@yahoo.com', 'supersecret2');
-insert into user values (0, 'bcurry', 'baconcurry@gmail.com', 'supersecret3');
-insert into user values (0, 'kmack', 'mackattack@valdosta.edu', 'supersecret4');
-insert into user values (0, 'dklein', 'inkleined@madisoncollege.edu', 'supersecret5');
-insert into user values (0, 'dtillman', 'xxtillmanxx@hotmail.com', 'supersecret6');
+insert into user values (1, 'jcoyne', 'coynemcgoin@hotmail.com', 'supersecret1');
+insert into user values (2, 'fhensen', 'scoobydoolover@yahoo.com', 'supersecret2');
+insert into user values (3, 'bcurry', 'baconcurry@gmail.com', 'supersecret3');
+insert into user values (4, 'kmack', 'mackattack@valdosta.edu', 'supersecret4');
+insert into user values (5, 'dklein', 'inkleined@madisoncollege.edu', 'supersecret5');
+insert into user values (6, 'dtillman', 'xxtillmanxx@hotmail.com', 'supersecret6');
