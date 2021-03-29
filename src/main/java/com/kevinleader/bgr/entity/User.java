b91e1a76094a0ae1bd3e975c1cc9447f9ea78091
kileader <kevin.i.leader@gmail.com>
2021-03-28 21:42:53 -0400

    Get Role.java working

--- src/main/java/com/kevinleader/bgr/entity/User.java
+++ src/main/java/com/kevinleader/bgr/entity/User.java
@@ -33,8 +33,8 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private Set<RankingConfiguration> rankingConfigurations = new HashSet<>();
 
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
-//    private Set<Role> roles = new HashSet<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
 
     /**
      * Instantiates a new User with no parameters.
